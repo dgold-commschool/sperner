15efae6fd56c12273471787da74494ba687e0dcf
dgold@commschool <dgold@commschool.org>
2025-04-15 12:14:19 -0400

    added counter to corner

--- game/sperner_pygame.py
+++ game/sperner_pygame.py
@@ -12,7 +12,7 @@ FramePerSec = pygame.time.Clock()
 width, height = 640, 640
 screen = pygame.display.set_mode((width, height))
 
-n = 20
+n = 6
 x_buff, y_buff = 20, 20
 dot_radius = 5
 point_buff = (width - (2 * x_buff) - (n * 2 * dot_radius)) / (n - 1)
@@ -41,6 +41,8 @@ color_lookup = {"r": (255, 0, 0),
                 "b": (0, 0, 255),
                 ".": (255, 255, 255)}
 
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+cheats = False
 current_color = "r"
 blah = True
 while True:
@@ -61,6 +63,7 @@ while True:
             if row < n - 1:
                 corners = triangle[row][index] + triangle[row + 1][index] + triangle[row+1][index+1]
                 if "r" in corners and "g" in corners and "b" in corners:
+                    poly_count += 1
                     moved_center = (centers[row][index][0], centers[row][index][1] + point_buff / 2 + 2 * dot_radius)
                     pygame.draw.circle(screen, (255, 0, 255), moved_center, dot_radius)
             if row > 0 and 0 < index and index < row:
@@ -69,6 +72,9 @@ while True:
                     poly_count += 1
                     moved_center = (centers[row][index][0], centers[row][index][1] - point_buff / 2 - 2 * dot_radius)
                     pygame.draw.circle(screen, (255, 0, 255), moved_center, dot_radius)
+
+    text_surface = my_font.render(str(poly_count), False, (255, 0, 255))
+    screen.blit(text_surface, (width - 4 * x_buff, y_buff))
 
 
     for event in pygame.event.get():
@@ -85,6 +91,8 @@ while True:
                 current_color = {"r": "g", "g": "b", "b": "r"}[current_color]
             if event.key == pygame.K_p:
                 triangle = random_triangle(n)
+            if event.key == pygame.K_c:
+                cheats = True
                 
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -96,7 +104,7 @@ while True:
                     dist = math.hypot(center[0] - x, center[1] - y)
                     if dist <= dot_radius:
                         if (index != 0 or current_color in "rg") and (index != row or current_color in "rb") and (row != n - 1 or current_color in "gb"):
-                            if triangle[row][index] == ".":
+                            if triangle[row][index] == "." or cheats:
                                 triangle[row] = triangle[row][:index] + current_color + triangle[row][index+1:]
                             else:
                                 print("already color")
