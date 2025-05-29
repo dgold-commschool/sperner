7c1692943201a3a6b3464e5a200cc256aa2439ec
dgold@commschool <dgold@commschool.org>
2025-05-29 15:39:50 -0400

    added colorblind friendliness

--- game/sperner_pygame.py
+++ game/sperner_pygame.py
@@ -6,15 +6,18 @@ pygame.init()
 font = pygame.font.SysFont(None, 24)
 clock = pygame.time.Clock()
 
+small_font = pygame.font.SysFont("verdana", 36, True)
+text_offset = 13
+
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
 width, height = 640, 640
 screen = pygame.display.set_mode((width, height))
 
-n = 6
+n = 5
 x_buff, y_buff = 20, 20
-dot_radius = 5
+dot_radius = 30
 point_buff = (width - (2 * x_buff) - (n * 2 * dot_radius)) / (n - 1)
 mid_x = (width - 2 * x_buff) / 2
 
@@ -34,28 +37,36 @@ for row in range(n):
 color_lookup = {"r": (255, 0, 0),
                 "g": (0, 255, 0),
                 "b": (0, 0, 255),
-                ".": (255, 255, 255)}
+                ".": (0, 0, 0)}
 
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 cheats = False
 current_color = "r"
 blah = True
 while True:
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
 
     pygame.draw.circle(screen, color_lookup[current_color], (x_buff, y_buff), dot_radius)
 
     for row in range(n):
-        pygame.draw.line(screen, (255, 255, 255), centers[row][0], centers[row][-1])            # Across
-        pygame.draw.line(screen, (255, 255, 255), centers[row][-1], centers[n-1][row])          # Down Left
-        pygame.draw.line(screen, (255, 255, 255), centers[row][0], centers[n-1][n - row - 1])   # Down Right  
+        pygame.draw.line(screen, (0, 0, 0), centers[row][0], centers[row][-1], 3)            # Across
+        pygame.draw.line(screen, (0, 0, 0), centers[row][-1], centers[n-1][row], 3)          # Down Left
+        pygame.draw.line(screen, (0, 0, 0), centers[row][0], centers[n-1][n - row - 1], 3)   # Down Right  
 
     poly_count = 0
     for row in range(n):
         for index in range(row + 1):
             center = centers[row][index]
-            pygame.draw.circle(screen, color_lookup[triangle[row][index]], center, dot_radius)
-            if row < n - 1:
+            if triangle[row][index] == ".":
+                pygame.draw.circle(screen, (255, 255, 255), center, dot_radius)
+                pygame.draw.circle(screen, (0, 0, 0), center, dot_radius, 3)
+            else:
+                pygame.draw.circle(screen, color_lookup[triangle[row][index]], center, dot_radius)
+                pygame.draw.circle(screen, (0, 0, 0), center, dot_radius, 3)
+                text_surface = small_font.render(triangle[row][index].upper(), False, (0, 0, 0))
+                screen.blit(text_surface, (center[0] - text_offset, center[1] - text_offset - 8))
+
+            """if row < n - 1:
                 corners = triangle[row][index] + triangle[row + 1][index] + triangle[row+1][index+1]
                 if "r" in corners and "g" in corners and "b" in corners:
                     poly_count += 1
@@ -66,7 +77,7 @@ while True:
                 if "r" in corners and "g" in corners and "b" in corners:
                     poly_count += 1
                     moved_center = (centers[row][index][0], centers[row][index][1] - point_buff / 2 - 2 * dot_radius)
-                    pygame.draw.circle(screen, (255, 0, 255), moved_center, dot_radius)
+                    pygame.draw.circle(screen, (255, 0, 255), moved_center, dot_radius)"""
 
     text_surface = my_font.render(str(poly_count), False, (255, 0, 255))
     screen.blit(text_surface, (width - 4 * x_buff, y_buff))
